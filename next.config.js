4209d55f38e482e1d90379a39b0e92e15b9b50a2
3210jr <3210jr@gmail.com>
2023-04-24 13:50:52 -0700

    remove image optimization

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const withMarkdoc = require('@markdoc/next.js')
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md'],
+  experimental: {
+    scrollRestoration: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = withMarkdoc()(nextConfig)

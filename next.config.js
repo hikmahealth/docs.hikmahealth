6231e4a2a2e482a05ce26f80b238475620b26d2a
3210jr <3210jr@gmail.com>
2023-04-24 13:23:43 -0700

    optimize image rendering with next js

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ const withMarkdoc = require('@markdoc/next.js')
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md'],
-  experimental: {
-    scrollRestoration: true,
-  },
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = withMarkdoc()(nextConfig)
